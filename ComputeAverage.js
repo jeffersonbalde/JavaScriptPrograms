92f23697a6d9ba06259c1431d6164cdaaae9aaa4
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-06-11 20:00:10 +0800

    compute average javascript program

---
+++ ComputeAverage.js
@@ -0,0 +1,30 @@
+/* 
+
+    Write a JavaScript program that computes the average marks of the following students
+
+*/
+
+
+let students = [['David', 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
+
+let Avgmarks = 0;
+let avg;
+
+for (let i=0; i < students.length; i++) {
+        Avgmarks += students[i][1];
+        avg = (Avgmarks/students.length);
+}
+
+console.log("Average grade: " + (Avgmarks)/students.length);
+
+if (avg < 60){
+    console.log("Grade : F");      
+} else if (avg < 70) {
+    console.log("Grade : D"); 
+}else if (avg < 80) {
+    console.log("Grade : C"); 
+}else if (avg < 90) {
+    console.log("Grade : B"); 
+}else if (avg < 100) {
+    console.log("Grade : A"); 
+}
