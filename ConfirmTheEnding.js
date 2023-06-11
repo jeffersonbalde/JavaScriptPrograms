22cad98e4b0c627cc402483a9986a21218e44c19
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-06-11 20:00:26 +0800

    compute average javascript program

---
+++ ConfirmTheEnding.js
@@ -0,0 +1,12 @@
+<!-- 
+
+    freeCodeCamp Task:
+    Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+ -->
+
+    function confirmEnding(str, target) {
+        return str.slice(str.length - target.length) === target;
+    }
+
+    console.log(confirmEnding("He has to give me a new name", "name"));
