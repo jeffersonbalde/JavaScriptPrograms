53677eabcba6e9d4fe27258652e4b654fccf3ff5
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-06-18 23:21:01 +0800

    current date javascript program

---
+++ CurrentDate.js
@@ -0,0 +1,30 @@
+/* 
+
+    Write a JavaScript program to get the current date.
+
+*/
+
+let today = new Date();
+let dd = today.getDate();
+
+let mm = today.getMonth()+1; 
+const yyyy = today.getFullYear();
+if(dd<10) {
+    dd=`0${dd}`;
+} 
+
+if(mm<10) {
+    mm=`0${mm}`;
+} 
+
+today = `${mm}-${dd}-${yyyy}`;
+console.log(today);
+
+today = `${mm}/${dd}/${yyyy}`;
+console.log(today);
+
+today = `${dd}-${mm}-${yyyy}`;
+console.log(today);
+
+today = `${dd}/${mm}/${yyyy}`;
+console.log(today);
