df159c1c63bae5a411aea2e71a013b9ae24b8e0c
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-06-28 11:36:31 +0800

    diff two arrays javascript program

--- DiffTwoArrays.js
+++ DiffTwoArrays.js
@@ -3,22 +3,8 @@
 // the symmetric difference of the two arrays.
 
 function diffArray(arr1, arr2) {
-    const newArr = [];
-  
-    function onlyInFirst(first, second) {
-      // Looping through an array to find elements that don't exist in another array
-      for (let i = 0; i < first.length; i++) {
-        if (second.indexOf(first[i]) === -1) {
-          // Pushing the elements unique to first to newArr
-          newArr.push(first[i]);
-        }
-      }
-    }
-  
-    onlyInFirst(arr1, arr2);
-    onlyInFirst(arr2, arr1);
-  
-    return newArr;
+    return arr1
+      .concat(arr2)
+      .filter(item => !arr1.includes(item) || !arr2.includes(item));
 }
-  
 console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
