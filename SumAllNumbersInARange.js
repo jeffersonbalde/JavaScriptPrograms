e50d78d7a2d940537feea1923a239acfaa9d0f30
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-06-28 09:08:42 +0800

    Sum all numbers in a range JavaScript program

--- SumAllNumbersInARange.js
+++ SumAllNumbersInARange.js
@@ -6,7 +6,11 @@ function sumAll(arr){
     let min = Math.min(arr[0], arr[1]);
     let sumOfAll = 0;
 
-    for(let i = min; i <= max; i++){
+    // for(let i = min; i <= max; i++){
+    //     sumOfAll += i;
+    // }
+
+    for(let i = Math.min(...arr); i <= Math.max(...arr); i++){
         sumOfAll += i;
     }
 
