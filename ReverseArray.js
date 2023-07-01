b5997a30d396276bbcc65a668164260a39975a75
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-07-01 23:51:25 +0800

    reverse array javascript program

--- ReverseArray.js
+++ ReverseArray.js
@@ -1,8 +1,10 @@
 /*
- * Complete the 'reverseArray' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts INTEGER_ARRAY a as parameter.
+
+  Complete the 'reverseArray' function below.
+ 
+  The function is expected to return an INTEGER_ARRAY.
+  The function accepts INTEGER_ARRAY a as parameter.
+  
  */
 
 function reverseArray(a) {
