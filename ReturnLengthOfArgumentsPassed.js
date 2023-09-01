08eab0edb77932aea44ff77f0f13b368a735eead
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-09-01 01:00:46 -0700

    return length of argument passed javascript program

---
+++ ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,14 @@
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+console.log(argumentsLength(1,2,3,4,5))
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
